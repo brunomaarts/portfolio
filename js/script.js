b330f5b8371f567f55e248fa9a3bf1dc5990fd21
Bruno Ricardo <zbrunodx@gmail.com>
2020-03-14 17:58:08 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+	$(window).scroll(function () {
+		var topPos = $(window).scrollTop();
+		if (topPos > 10) {
+			$('header').addClass('active');
+		} else {
+			$('header').removeClass('active');
+		}
+	});
+
+	$('.box-menu').on('click', function () {
+		$('header').toggleClass('active-nav');
+	});
+
+	$('a[href*="#"]')
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		.click(function (event) {
+			if (
+				location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+				&&
+				location.hostname == this.hostname
+			) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					event.preventDefault();
+					$('html, body').animate({
+						scrollTop: target.offset().top -83
+					}, 800);
+				}
+			}
+		});
+});
